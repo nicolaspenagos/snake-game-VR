c247ed66e9b3d5a77baf86e4cbeead2974fe7e89
nicolaspenagos <nicolas.penagosm98@gmail.com>
2022-04-16 17:56:09 +0200

    Snake now is moving

--- Assets/Scenes/Model/BoardGame.cs
+++ Assets/Scenes/Model/BoardGame.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
+using UnityEngine;
 namespace Model{
 public class BoardGame
     {
@@ -8,6 +10,9 @@ public class BoardGame
         private Queue<Pair> snake;
         private int boardGameSize = 15;
         private float cubeSize = 0.3f;
+        private Thread movementThread;
+        private bool continueMoving;
+        private char direction;
 
 
         public BoardGame()
@@ -15,7 +20,16 @@ public class BoardGame
             boardGame = new Square[boardGameSize, boardGameSize];
             snake = new Queue<Pair>();
             snake.Enqueue(new Pair(7, 7));
+            continueMoving = true;
+            direction = Square.RIGHT;
+
             loadBoardGame();
+            setEnemy();
+
+            movementThread = new Thread(new ThreadStart(moveThread));
+            movementThread.Start();
+          
+
       
         }
 
@@ -75,7 +89,7 @@ public class BoardGame
             while (contains)
             {
 
-                Random rand = new Random();
+                System.Random rand = new System.Random();
                 int i = rand.Next(0, boardGameSize);
                 int j = rand.Next(0, boardGameSize);
   
@@ -108,11 +122,139 @@ public class BoardGame
 
         }
 
+        public void moveThread(){
+
+            while(continueMoving){
+                Thread.Sleep(400);
+                Debug.Log("Hola!");
+                move();
+            }
+           
+        }
+
+        public void move(){
+
+
+            Queue<Pair> temp = new Queue<Pair>();
+            Pair p = snake.Peek();
+            int i = p.getFirst();
+            int j = p.getSecond();
+
+            switch (direction)
+            {
+
+                case Square.UP:
+                    j--;
+                    break;
+
+                case Square.DOWN:
+                    j++;
+                    break;
+
+                case Square.LEFT:
+                    i--;
+                    break;
+
+                case Square.RIGHT:
+                    i++;
+                    break;
+
+            }
+
+            int limit = 1;
+
+            if (boardGame[i,j].getCurrentColor() == Square.RED)
+            {
+                limit = 0;
+                setEnemy();
+            }
+            else if (boardGame[i,j].getCurrentColor() == Square.GREEN)
+            {
+                continueMoving = false;
+                //PERDIOOO --------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            }
+
+            temp.Enqueue(new Pair(i, j));
+
+            int tailI = 0;
+            int tailJ = 0;
+
+            while (snake.Count > limit)
+            {
+
+                p = snake.Dequeue();
+                temp.Enqueue(p);
+                tailI = p.getFirst();
+                tailJ = p.getSecond();
+
+            }
+
+            if (snake.Count>0)
+            {
+
+                p = snake.Dequeue();
+                i = p.getFirst();
+                j = p.getSecond();
+                boardGame[i,j].setSnake(false);
+
+            }
+            else
+            {
+                if (tailI < (boardGameSize-1) && tailI > 0 && tailJ < (boardGameSize-1) && tailJ > 0)
+                {
+
+                    if (direction == Square.UP)
+                    {
+                        tailJ++;
+                        boardGame[tailI,tailJ].setSnake(false);
+                    }
+                    else if (direction == Square.DOWN)
+                    {
+                        tailJ--;
+                        boardGame[tailI,tailJ].setSnake(false);
+                    }
+                    else if (direction == Square.RIGHT)
+                    {
+                        tailI--;
+                        boardGame[tailI,tailJ].setSnake(false);
+                    }
+                    else
+                    {
+                        tailI++;
+                        boardGame[tailI,tailJ].setSnake(false);
+                    }
+
+                }
+
+            }
+
+            snake = temp;
+
+           
+            
+            foreach (Pair pair in snake)
+            {
+                i = pair.getFirst();
+                j = pair.getSecond();
+
+                boardGame[i,j].setSnake(true);
+            }
+            p = snake.Peek();
+            i = p.getFirst();
+            j = p.getSecond();
+
+            boardGame[i,j].setCurrentColor(Square.DARK_GREEN);
+        }
+
     
 
         public Square[,] getBoardGame()
         {
             return boardGame;
+        }
+
+        public int getBoardGameSize(){
+            return boardGameSize;
         }
 
 
--- Assets/Scenes/Rotate.cs
+++ Assets/Scenes/Rotate.cs
@@ -10,41 +10,37 @@ public class Rotate : MonoBehaviour
 	private bool rotate;
     private BoardGame boardGame;
     private Square[,] matrix;
+    private GameObject[,] cubesMatrix;
 	MeshRenderer rend;
 	Color original;
     // Start is called before the first frame update
     void Start()
     {
+
+
+
     	rotate = true;
     	rend = GetComponentInChildren<MeshRenderer>();
     	original = rend.material.color;
 
         // DRAW DE MATRIX 
+        
         boardGame = new BoardGame();
+        cubesMatrix = new GameObject[boardGame.getBoardGameSize(), boardGame.getBoardGameSize()];
         matrix = boardGame.getBoardGame();
-
 
         for(int i=0; i<matrix.GetLength(0); i++){
             for(int j=0; j<matrix.GetLength(1); j++){
-
                 Square cube = matrix[i,j];
-       
-                Debug.Log(cube.getPosX()+" "+cube.getPosY());
-
-                
                 GameObject cubeObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
                 cubeObject.transform.localPosition = new Vector3(cube.getPosX(), cube.getPosY(), -1);
                 cubeObject.transform.localScale = new Vector3(cube.getSize(), cube.getSize(), cube.getSize());
                 cubeObject.GetComponent<MeshRenderer>().material.color = getColor(cube.getCurrentColor());
+                cubesMatrix[i,j] = cubeObject;
             }
         }
 
-        
-
-
-      
-
+        //
     }
 
     // Update is called once per frame
@@ -53,6 +49,19 @@ public class Rotate : MonoBehaviour
     	if(rotate){
     		 transform.Rotate(transform.up * Time.deltaTime * 45f, Space.Self);
     	}
+
+        for (int i = 0; i < cubesMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < cubesMatrix.GetLength(1); j++)
+            {
+                Square cube = matrix[i, j];
+                GameObject cubeObject = cubesMatrix[i, j];
+                cubeObject.GetComponent<MeshRenderer>().material.color = getColor(cube.getCurrentColor());
+
+              
+            }
+        }
+        
        
     }
 
@@ -65,6 +74,12 @@ public class Rotate : MonoBehaviour
 
         if (c == Square.DARK_GREEN)
             return new Color((158 / 255.0f), (178 / 255.0f), (55 / 255.0f));
+
+        if (c == Square.GREEN)
+            return new Color((158 / 255.0f), (178 / 255.0f), (55 / 255.0f));
+
+        if (c == Square.RED)
+            return new Color((185 / 255.0f), (46 / 255.0f), (77 / 255.0f));
 
         return Color.red;
     }
